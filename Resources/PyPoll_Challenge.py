e31c7eff00e0ea00d679e2e9f151256ce1b45165
kaalrok <92065902+kaalrok@users.noreply.github.com>
2021-12-27 09:35:26 +0100

    updating folders

--- Resources/PyPoll_Challenge.py
+++ Resources/PyPoll_Challenge.py
@@ -1,14 +1,21 @@
 # -*- coding: UTF-8 -*-
 """PyPoll Homework Challenge Solution."""
 
+# The data requested by the stakeholder:
+# 1. The total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on popular vote
+
 # Add our dependencies.
 import csv
 import os
 
 # Add a variable to load a file from a path.
-file_to_load = os.path.join("..", "Resources", "election_results.csv")
+file_to_load = os.path.join(".", "Resources", "election_results.csv")
 # Add a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join(".", "Analysis", "election_analysis.txt")
 
 # Initialize a total vote counter.
 total_votes = 0
